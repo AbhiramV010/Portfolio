82955a749defb087ec22dfc889c3a03c6154000e
Abhiram Vadali <abhiram.vadali@gmail.com>
2026-08-18 22:22:38 -0400

    changed flow

--- src/App.jsx
+++ src/App.jsx
@@ -358,7 +358,7 @@ export default function Home() {
               </svg>
             </a>
 
-            <a href="https://www.linkedin.com/in/abhiram-vadali-286a56361/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" style={{ display: "flex" }}>
+            <a href="https://www.linkedin.com/in/abhiramv010/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" style={{ display: "flex" }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                 <rect width="4" height="12" x="2" y="9"></rect>
